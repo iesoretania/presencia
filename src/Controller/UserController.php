cd86815fcb0ca851f44350e089ce32a8161ae8c5
Luis Ramón López López <lrlopez@gmail.com>
2020-09-03 00:29:29 +0200

    Permitir eliminar usuarios

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,18 +2,10 @@
 
 namespace App\Controller;
 
-use App\Entity\Presence\AccessCode;
 use App\Entity\User;
-use App\Entity\Worker;
-use App\Form\AccessCodeEditType;
-use App\Form\ImportType;
-use App\Form\Model\FileImport;
 use App\Form\UserEditType;
 use App\Form\UserNewType;
-use App\Repository\Presence\AccessCodeRepository;
 use App\Repository\UserRepository;
-use App\Repository\WorkerRepository;
-use App\Service\ImportService;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -100,117 +92,33 @@ class UserController extends AbstractController
     }
 
     /**
-     * @Route("/personal/eliminar/{id}", name="worker_delete", requirements={"id":"\d+"})
+     * @Route("/eliminar/{id}", name="user_delete", requirements={"id":"\d+"})
      */
-    public function workerDeleteAction(
+    public function userDeleteAction(
         Request $request,
-        WorkerRepository $workerRepository,
+        UserRepository $userRepository,
         TranslatorInterface $translator,
-        Worker $worker
+        User $user
     ): Response
     {
+        if ($user === $this->getUser()) {
+            throw $this->createAccessDeniedException();
+        }
+
         if ($request->get('confirm', '') === 'ok') {
             try {
-                $workerRepository->delete($worker);
-                $this->addFlash('success', $translator->trans('message.deleted', [], 'worker'));
+                $userRepository->delete($user);
+                $this->addFlash('success', $translator->trans('message.deleted', [], 'user'));
             } catch (\Exception $e) {
-                $this->addFlash('error', $translator->trans('message.delete_error', [], 'worker'));
+                $this->addFlash('error', $translator->trans('message.delete_error', [], 'user'));
             }
             return $this->redirectToRoute(
-                'worker'
+                'user'
             );
         }
 
-        return $this->render('worker/delete.html.twig', [
-            'worker' => $worker
-        ]);
-    }
-
-    /**
-     * @Route("/personal/importar", name="worker_import")
-     */
-    public function workerImportAction(
-        Request $request,
-        TranslatorInterface $translator,
-        ImportService $importService
-    ): Response
-    {
-        $csvFile = new FileImport();
-
-        $form = $this->createForm(ImportType::class, $csvFile);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            try {
-                $importService->importFromFile($csvFile->file->getPathname());
-
-                return $this->redirectToRoute('worker');
-            } catch (\RuntimeException $e) {
-                $this->addFlash('error', $translator->trans('message.bad_format', [], 'worker'));
-            } catch (\Exception $e) {
-                $this->addFlash('error', $translator->trans('message.save_error', [], 'worker'));
-            }
-        }
-
-        return $this->render('worker/import_form.html.twig', [
-            'form' => $form->createView()
-        ]);
-    }
-
-    /**
-     * @Route("/personal/codigo/{id}", name="worker_access_code_form", requirements={"id":"\d+"})
-     */
-    public function workerAccessCodeFormAction(
-        Request $request,
-        AccessCodeRepository $accessCodeRepository,
-        TranslatorInterface $translator,
-        AccessCode $accessCode
-    ): Response
-    {
-        $form = $this->createForm(AccessCodeEditType::class, $accessCode);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            try {
-                $accessCodeRepository->save($accessCode);
-                $this->addFlash('success', $translator->trans('message.saved', [], 'worker'));
-                return $this->redirectToRoute('worker_form', ['id' => $accessCode->getWorker()->getId()]);
-            } catch (\Exception $e) {
-                $this->addFlash('error', $translator->trans('message.save_error', [], 'worker'));
-            }
-        }
-
-        return $this->render('worker/access_code_form.html.twig', [
-            'form' => $form->createView(),
-            'access_code' => $accessCode
-        ]);
-    }
-
-    /**
-     * @Route("/personal/codigo/eliminar/{id}", name="worker_access_code_delete", requirements={"id":"\d+"})
-     */
-    public function workerAccessCodeDeleteAction(
-        Request $request,
-        AccessCodeRepository $accessCodeRepository,
-        TranslatorInterface $translator,
-        AccessCode $accessCode
-    ): Response
-    {
-        if ($request->get('confirm', '') === 'ok') {
-            try {
-                $accessCodeRepository->delete($accessCode);
-                $this->addFlash('success', $translator->trans('message.access_code_deleted', [], 'worker'));
-            } catch (\Exception $e) {
-                $this->addFlash('error', $translator->trans('message.access_code_delete_error', [], 'worker'));
-            }
-            return $this->redirectToRoute(
-                'worker_form',
-                ['id' => $accessCode->getWorker()->getId()]
-            );
-        }
-
-        return $this->render('worker/access_code_delete.html.twig', [
-            'access_code' => $accessCode
+        return $this->render('user/delete.html.twig', [
+            'user' => $user
         ]);
     }
 }
